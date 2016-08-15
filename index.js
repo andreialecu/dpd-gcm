e76dedf36818fcaee5a3acc288a111b4e994723f
Andrei Alecu <andreialecu@users.noreply.github.com>
2016-08-15 16:35:49 +0300

    Chunk sends by 1000 at a time (gcm limit)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Resource = require('deployd/lib/resource');
-var util     = require('util');
-var gcm      = require('node-gcm');
+var util = require('util');
+var gcm = require('node-gcm');
+var _ = require('lodash');
+var async = require('async');
 
-function dpdgcm( options ) {
+function dpdgcm(options) {
     Resource.apply(this, arguments);
     this.gcmsender = new gcm.Sender(this.config.APIKey);
 }
@@ -15,59 +17,67 @@ dpdgcm.events = ["post"];
 dpdgcm.prototype.clientGeneration = true;
 
 dpdgcm.basicDashboard = {
-    settings: [
-        {
-            name        : 'APIKey',
-            type        : 'string',
-            description : 'API Key'
-        },
-        {
-            name        : 'defaultTitle',
-            type        : 'string',
-            description : 'Default Title'
-        }
-    ]
+    settings: [{
+        name: 'APIKey',
+        type: 'string',
+        description: 'API Key'
+    }, {
+        name: 'defaultTitle',
+        type: 'string',
+        description: 'Default Title'
+    }]
 };
 
-dpdgcm.prototype.handle = function ( ctx, next ) {
+dpdgcm.prototype.handle = function(ctx, next) {
 
     var devices;
     var title, message;
 
-    if(ctx.body && ctx.body.devices){
+    if (ctx.body && ctx.body.devices) {
         devices = ctx.body.devices;
     }
 
-    if(ctx.body && ctx.body.title){
+    if (ctx.body && ctx.body.title) {
         title = ctx.body.title;
-    }else{
+    } else {
         title = this.config.defaultTitle;
     }
 
-    if(ctx.body && ctx.body.message){
+    if (ctx.body && ctx.body.message) {
         message = ctx.body.message;
     }
 
     var msg = new gcm.Message();
 
     msg.delay_while_idle = false;
-    msg.addData("title",title);
-    msg.addData("message",message);
+    msg.addData("title", title);
+    msg.addData("message", message);
     msg.addData('sound', 'default');
 
     if (ctx.body.payload) {
-      for (var key in ctx.body.payload) {
-        msg.addData(key, ctx.body.payload[key]);
-      }
+        for (var key in ctx.body.payload) {
+            msg.addData(key, ctx.body.payload[key]);
+        }
     }
 
-    this.gcmsender.send(msg, devices, 5, function(err, result) {
-        if(err){
+    var deviceChunks = _.chunk(devices, 1000);
+    var results = [];
+    var _this = this;
+    async.each(deviceChunks, function(devices, callback) {
+        _this.gcmsender.send(msg, devices, 5, function(err, result) {
+            callback(err, result);
+            if (result) {
+                results.push(result);
+            }
+        });
+    }, function(err) {
+        if (err) {
             ctx.done(err);
-        }else{
-            ctx.done(null, result);
+        } else {
+            ctx.done(null, results);
         }
     });
+
 };
 
 module.exports = dpdgcm;
